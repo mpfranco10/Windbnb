95401b21f361f87bf101fa035a4e53757f73129f
Maria <mpfrancog@outlook.com>
2023-07-26 21:41:25 -0500

    feat: changed filter structure, fixed some styles

--- src/components/header/filters/FilterDetails.tsx
+++ src/components/header/filters/FilterDetails.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
-import SmallFilter from "./SmallFilter";
+import SearchIcon from "@mui/icons-material/Search";
+import CloseIcon from "@mui/icons-material/Close";
 import "./styles/FilterDetails.css";
 type FilterDetailsProps = { onClick?: () => void };
 
-export default function FilterDetails({ onClick }: FilterDetailsProps) {
+export default function FilterDetails({
+  onClick: onToggleClick,
+}: FilterDetailsProps) {
   const [filterShown, setFilterShown] = useState<"location" | "guests">(
     "location",
   );
@@ -11,11 +14,25 @@ export default function FilterDetails({ onClick }: FilterDetailsProps) {
   return (
     <div className="filterDetails">
       <div className="filterDetailsContent">
-        <SmallFilter
-          showLabels={true}
-          searchButtonType="big"
-          onCloseClick={onClick}
-        />
+        <div className="bigFilter">
+          <div className="bigFilterItem">
+            <p className="filterLabel">LOCATION</p>
+            Helsinki, Finland
+          </div>
+          <div className="bigFilterItem grayFilterText">
+            <p className="filterLabel">GUESTS</p>
+            Add guests
+          </div>
+          <div className="filterItemButton fullHeight">
+            <button className="bigSearchButton">
+              <SearchIcon style={{ fill: "white" }} />
+              Search
+            </button>
+            <div className="closeButton">
+              <CloseIcon onClick={onToggleClick} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/header/filters/SmallFilter.tsx
+++ src/components/header/filters/SmallFilter.tsx
@@ -1,58 +1,17 @@
 import "./styles/SmallFilter.css";
 import SearchIcon from "@mui/icons-material/Search";
-import CloseIcon from "@mui/icons-material/Close";
-
-const SMALL_BUTTON_TYPE = "small";
-const BIG_BUTTON_TYPE = "big";
 
 type SmallFilterProps = {
-  onClick?: () => void;
-  onLocationClick?: () => void;
-  onGuestsClick?: () => void;
-  onSearchClick?: () => void;
-  onCloseClick?: () => void;
-  showLabels?: boolean;
-  searchButtonType?: "big" | "small";
+  onClick: () => void;
 };
 
-export default function SmallFilter({
-  onClick,
-  onLocationClick,
-  onGuestsClick,
-  onSearchClick,
-  showLabels = false,
-  searchButtonType = SMALL_BUTTON_TYPE,
-  onCloseClick,
-}: SmallFilterProps) {
-  const noClick = () => {};
+export default function SmallFilter({ onClick }: SmallFilterProps) {
   return (
-    <div className="smallFilter" onClick={onClick ?? noClick}>
-      <div className="filterItem" onClick={onLocationClick ?? noClick}>
-        {showLabels && <p className="filterLabel">LOCATION</p>}
-        Helsinki, Finland
-      </div>
-      <div
-        className="filterItem grayFilterText"
-        onClick={onGuestsClick ?? noClick}
-      >
-        {showLabels && <p className="filterLabel">GUESTS</p>}
-        Add guests
-      </div>
-      <div
-        className="filterItemButton fullHeight"
-        onClick={onSearchClick ?? noClick}
-      >
-        {searchButtonType === SMALL_BUTTON_TYPE ? (
-          <SearchIcon style={{ fill: "#f05454" }} />
-        ) : (
-          <>
-            <button className="bigSearchButton">
-              <SearchIcon style={{ fill: "white" }} />
-              Search test
-            </button>
-            <CloseIcon onClick={onCloseClick} />
-          </>
-        )}
+    <div className="smallFilter" onClick={onClick}>
+      <div className="filterItem">Helsinki, Finland</div>
+      <div className="filterItem grayFilterText">Add guests</div>
+      <div className="filterItem">
+        <SearchIcon style={{ fill: "#f05454" }} />
       </div>
     </div>
   );
